da3e3fb8fd4afad65a6f94d239c033ee44c29dd9
Channthoeun Ken <channthoeun@gmail.com>
2016-04-23 12:01:28 +0700

    Added more lines

--- result.php
+++ result.php
@@ -1,4 +1,8 @@
 <?php
 	$r = 10 + 2;
+	$first_name = "Channthoeun";
+	$last_name = "Ken";
+	echo "<h1>Hello World!!!</h1>";
+	echo "<h2>I am " + $first_name + " " + $last_name + "</h2>";
 	echo "Result = " + $r;
 ?>
